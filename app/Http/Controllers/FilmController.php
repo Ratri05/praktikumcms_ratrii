fe0d0f28ea2d4fabcf973f36d03eaddbd7ecb757
Ratri05 <febriyantiratri@gmail.com>
2025-06-20 14:12:37 +0700

    Error Handler

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -4,23 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Film;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FilmController extends Controller
 {
+    // Untuk keperluan tugas: menampilkan data film menggunakan findOrFail()
+    public function showDetail($id)
+    {
+        $film = Film::findOrFail($id);
+        return view('films.show', compact('film'));
+    }
 
+    // Untuk keperluan tugas: menampilkan data film dengan try-catch
+    public function showWithTryCatch($id)
+    {
+        try {
+            $film = Film::findOrFail($id);
+            return view('films.show', compact('film'));
+        } catch (ModelNotFoundException $e) {
+            return response()->view('errors.notfound', [], 404);
+        }
+    }
+
+    // Index
     public function index()
     {
         $films = Film::all();
         return view('films.index', compact('films'));
     }
 
-
+    // Create
     public function create()
     {
         return view('films.create');
     }
 
-
+    // Store
     public function store(Request $request)
     {
         $request->validate([
@@ -37,21 +56,21 @@ class FilmController extends Controller
         return redirect()->route('films.index')->with('success', 'Film berhasil ditambahkan.');
     }
 
-
+    // Show
     public function show($id)
     {
         $film = Film::findOrFail($id);
         return view('films.show', compact('film'));
     }
 
-
+    // Edit
     public function edit($id)
     {
         $film = Film::findOrFail($id);
         return view('films.edit', compact('film'));
     }
 
-
+    // Update
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -69,7 +88,7 @@ class FilmController extends Controller
         return redirect()->route('films.index')->with('success', 'Film berhasil diperbarui.');
     }
 
-
+    // Destroy
     public function destroy($id)
     {
         $film = Film::findOrFail($id);
@@ -78,10 +97,10 @@ class FilmController extends Controller
         return redirect()->route('films.index')->with('success', 'Film berhasil dihapus.');
     }
 
+    // Konfirmasi Hapus
     public function confirmDelete($id)
-{
-    $film = Film::findOrFail($id);
-    return view('films.confirmDelete', compact('film'));
-}
-
+    {
+        $film = Film::findOrFail($id);
+        return view('films.confirmDelete', compact('film'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\TiketController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\ImageController;
 
-// Hapus salah satu route root agar tidak bentrok, misal arahkan ke home
+// Halaman awal diarahkan ke home
 Route::get('/', function () {
-    return view('home'); // ganti welcome jadi home kalau memang itu tujuannya
+    return view('home'); // pastikan file resources/views/home.blade.php ada
 });
 
 /*
@@ -32,7 +32,7 @@ Route::post('/studio', [StudioController::class, 'store'])->name('studio.store')
 Route::get('/studio/{id}', [StudioController::class, 'show'])->name('studio.show');
 Route::get('/studio/{id}/edit', [StudioController::class, 'edit'])->name('studio.edit');
 Route::put('/studio/{id}', [StudioController::class, 'update'])->name('studio.update');
-Route::get('/studio/{id}/delete', [StudioController::class, 'confirmDelete'])->name('studio.confirmDelete'); // custom route confirmDelete
+Route::get('/studio/{id}/delete', [StudioController::class, 'confirmDelete'])->name('studio.confirmDelete');
 Route::delete('/studio/{id}', [StudioController::class, 'destroy'])->name('studio.destroy');
 
 /*
@@ -42,15 +42,15 @@ Route::delete('/studio/{id}', [StudioController::class, 'destroy'])->name('studi
 */
 Route::resource('films', FilmController::class);
 
+// Tambahan: route khusus untuk pengujian error handler dengan findOrFail
+Route::get('/film/detail/{id}', [FilmController::class, 'showDetail'])->name('film.detail');
+
 /*
 |--------------------------------------------------------------------------
 | Pengguna
 |--------------------------------------------------------------------------
 */
-// Resource route untuk CRUD standar
 Route::resource('pengguna', PenggunaController::class);
-
-// Tambahkan route custom untuk confirmDelete karena tidak ada di resource default
 Route::get('pengguna/{id}/delete', [PenggunaController::class, 'confirmDelete'])->name('pengguna.confirmDelete');
 
 /*
@@ -81,7 +81,11 @@ Route::put('/transaksi/{id}', [TransaksiController::class, 'update'])->name('tra
 Route::get('/transaksi/{id}/delete', [TransaksiController::class, 'confirmDelete'])->name('transaksi.confirmDelete');
 Route::delete('/transaksi/{id}', [TransaksiController::class, 'destroy'])->name('transaksi.destroy');
 
+/*
+|--------------------------------------------------------------------------
+| Upload Gambar
+|--------------------------------------------------------------------------
+*/
 Route::get('/upload', [ImageController::class, 'create']);
 Route::post('/upload', [ImageController::class, 'store'])->name('image.upload');
 Route::delete('/upload/{id}', [ImageController::class, 'destroy'])->name('image.delete');
-
